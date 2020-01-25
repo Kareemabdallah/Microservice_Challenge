3c1fb4ca357f338853083e1667591bc29a27cc38
snake <kareemabdallah@hotmail.com>
2020-01-25 15:40:04 +0100

    Files updated

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # !/bin/sh
-docker build -t app1/build -f Docker .
+docker build -t app1/build . -f Dockerfile
 docker run --rm -p 9000:9000 app1/build  
-docker build -t app2-build -f Docker .
+docker build -t app2-build . -f Dockerfile
 docker run --rm -p 5000:5000 app2/build
