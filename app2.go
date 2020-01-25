1e0774dce0871dee73c5194d85075b86d2b4ea99
snake <kareemabdallah@hotmail.com>
2020-01-25 13:40:43 +0100

    Files updated

--- app2.go
+++ app2.go
@@ -6,37 +6,42 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type Articles struct {
+/*type Articles struct {
 	Articles []Article `json:"articles"`
 }
 
 type Article struct {
 	id      string `json:"id"`
 	message string `json:"message"`
-}
+}*/
 
-func main() {
-	resp, err := http.Get("http://localhost:9000/articles")
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	resp, err := http.Get("http://localhost:9000/")
+	//data := json.RawMessage(string(resp)) // declaring a data string parsing data as raw
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	//json.NewEncoder(w).decode(resp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var generic map[string]interface{}
+	defer resp.Body.Close()
+
+	//var generic map[string]interface{}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	json.Unmarshal([]byte(body), &generic)
-
-	str := fmt.Sprint(generic["articles"]) //converting data type interface to string
+	text := string(body)
+	json.Unmarshal([]byte(body), &resp)
+	str := fmt.Sprint(text) //converting data type interface to string
 	str1 := Reverse(str)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(str1)
-
-		http.ListenAndServe(":80", nil)
-	})
+	fmt.Printf(string(str1))
 
 }
 
@@ -46,4 +51,17 @@ func Reverse(s string) string {
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You are on the home page")
+}
+
+func main() {
+
+	r := mux.NewRouter() //.StrictSlash(true)
+	r.HandleFunc("/articles", handler).Methods("GET")
+	r.HandleFunc("/", homepage).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
