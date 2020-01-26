1fa7e0393a72627ac4ac4b11e20ce22aa536cd07
snake <kareemabdallah@hotmail.com>
2020-01-26 02:13:17 +0100

    Files updated

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ timestamp() {
 
 # Go build
 echo "🐋	$(timestamp): building image example:test"
-    docker build -t app1 -f Dockerfile .
+    docker build -t app1:test -f Dockerfile .
     docker run --rm -p 9000:9000 app1 
 
-    docker build -t app2 -f Dockerfile2 .
+    docker build -t app2:test -f Dockerfile2 .
     docker run --rm -p 7000:7000 app2
     
 
